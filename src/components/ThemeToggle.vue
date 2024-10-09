6297dbe78429af19d79c5221a137d774ede644a2
Jesus Pinar <not@email.com>
2024-10-09 19:45:33 +0200

    Update html render component

--- src/components/ThemeToggle.vue
+++ src/components/ThemeToggle.vue
@@ -11,5 +11,5 @@ function toggleTheme() {
 </script>
 
 <template>
-  <a :aria-label="isDark ? 'Dark Theme' : 'Light Theme'" nav-link dark:i-ri-moon-line i-ri-sun-line @click="toggleTheme" />
+  <span :aria-label="isDark ? 'Dark Theme' : 'Light Theme'" nav-link dark:i-ri-moon-line i-ri-sun-line @click="toggleTheme" />
 </template>
