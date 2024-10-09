464d482a5ac99f4e871f3ac0831659146ff8abb0
Jesus Pinar <not@email.com>
2024-10-09 12:30:48 +0200

    Remove transition

--- src/components/ThemeToggle.vue
+++ src/components/ThemeToggle.vue
@@ -5,42 +5,8 @@ const isDark = useDark()
 
 const toggleDark = useToggle(isDark)
 
-function toggleTheme(event: MouseEvent) {
-  const x = event.clientX
-  const y = event.clientY
-  const endRadius = Math.hypot(
-    Math.max(x, innerWidth - x),
-    Math.max(y, innerHeight - y),
-  )
-  // @ts-expect-error: Transition API
-  if (!document.startViewTransition) {
-    toggleDark()
-    return
-  }
-
-  // @ts-expect-error: Transition API
-  const transition = document.startViewTransition(async () => {
-    toggleDark()
-  })
-
-  transition.ready.then(() => {
-    const clipPath = [
-      `circle(0px at ${x}px ${y}px)`,
-      `circle(${endRadius}px at ${x}px ${y}px)`,
-    ]
-    document.documentElement.animate(
-      {
-        clipPath: isDark.value ? [...clipPath].reverse() : clipPath,
-      },
-      {
-        duration: 400,
-        easing: 'ease-in',
-        pseudoElement: isDark.value
-          ? '::view-transition-old(root)'
-          : '::view-transition-new(root)',
-      },
-    )
-  })
+function toggleTheme() {
+  toggleDark()
 }
 </script>
 
